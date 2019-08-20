7be360ccebe8c098726c465bfa0e5abd6a619d42
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-19 20:09:40 -0700

    ...

--- data/migrations/20190817132531_create-table.js
+++ data/migrations/20190817132531_create-table.js
@@ -54,6 +54,7 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
     return knex.schema
+    .dropTableIfExists('project_resource')
     .dropTableIfExists('task')
     .dropTableIfExists('resource')
     .dropTableIfExists('project');
