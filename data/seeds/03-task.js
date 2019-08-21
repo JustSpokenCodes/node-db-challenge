0aabc17bcd7a1a822b108939f9525e03344ef3f3
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-20 22:10:01 -0700

    other words turned into numbers

--- data/seeds/03-task.js
+++ data/seeds/03-task.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   return knex('task').insert([
-    {description: 'Try and figure out what exactly Knex is and why we need it', notes: 'I think im starting to understand...nope nope nevermind', compeleted: false , project_id: 1},
-    {description: 'A great way to introduce someone new to async and promises', notes: 'When I used async in my code, it not only helped my code so I could follow a certain method but caused me headaches because all pieces of the promise have to be in every function that uses async', compeleted: true, project_id: 2},
-    {description: 'An introduction to UI and creatively creating your own', notes: 'This is a lot of fun because I can be creative and I finally know what Im seeing and doing', compeleted: false, project_id: 3},
-    {description: 'A test of will on creating your own middleware correctly', notes: 'I can finish this in no time...Oh Is It Really Midnight?!', compeleted: true, project_id: 4 }
+    {description: 'Try and figure out what exactly Knex is and why we need it', notes: 'I think im starting to understand...nope nope nevermind', compeleted: 0 , project_id: 1},
+    {description: 'A great way to introduce someone new to async and promises', notes: 'When I used async in my code, it not only helped my code so I could follow a certain method but caused me headaches because all pieces of the promise have to be in every function that uses async', compeleted: 1, project_id: 2},
+    {description: 'An introduction to UI and creatively creating your own', notes: 'This is a lot of fun because I can be creative and I finally know what Im seeing and doing', compeleted: 0, project_id: 3},
+    {description: 'A test of will on creating your own middleware correctly', notes: 'I can finish this in no time...Oh Is It Really Midnight?!', compeleted: 1, project_id: 4 }
   ]);
 };
 
